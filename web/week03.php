21963f0ca0f787f78f31ac356b65d2f114054f23
winsonso <winsonsosingwai319@gmail.com>
2017-01-20 08:47:26 -0700

    assignment03 testing

---
+++ web/week03.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Survey Result</title>
+</head>
+<body>
+<?php
+// session_start();
+// if(isset( $_SESSION['counter'] ) ) {
+//       echo "<center> You have already voted </center> ";
+//    }else {
+//      $_SESSION['counter'] = 1;
+   
+    $gender = $_POST["gender"];
+    $age = $_POST["age"];
+    $occuption = $_POST["occuption"];
+    $interest = $_POST["interest"];
+
+    echo "string ".$gender;
+//     $var = 0;
+//     $result = fopen("result.txt","w") or die("Unable to open file!!");
+
+// while(!feof($result)){
+//    $array[$var] = fgets($result);
+//    $var ++;
+// }
+// fclose($result);
+// $t1 = ($array[0]+$happy);
+// $t2 = ($array[1] + $hunger);
+// $t3 = ($array[2] + $car);
+// $t4 = ($array[3] +$student);
+// echo "<center>Result</center>";
+// echo "<br/><center>People who are happy: ". $t1 ."</center>";
+// echo "<br/><center>People who are happy: ". $t2 ."</center>";
+// echo "<br/><center>People who are happy: ". $t3 ."</center>";
+// echo "<br/><center>People who are happy: ". $t4 ."</center>";
+// $file = fopen("result.txt","w") or die("can't open fine");
+// $final = ($t1 . "\n" . $t2 . "\n" . $t3 . "\n" . $t4 . "\n");
+// fwrite($file,$final);
+// fclose($file);
+   //}
+?>
+</body>
+</html>
+
