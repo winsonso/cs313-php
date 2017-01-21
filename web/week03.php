9c9d545d1aad51b2c6bbe3178346e966aa00c675
winsonso <winsonsosingwai319@gmail.com>
2017-01-21 11:02:32 -0700

    fixed

--- web/week03.php
+++ web/week03.php
@@ -18,19 +18,12 @@ session_start();
     $age = $_POST["age"];
     $occuption = $_POST["occuption"];
     $interest = $_POST["interest"];
-// $gender="female";
-// $age="yes";
-// $occuption="yes";
-// $interest="yes";
 
 $space = " ";
 $gender = $gender.$space;;
 $age = $age.$space;;
 $occuption = $occuption.$space;;
 $interest = $interest.$space;;
-
-// echo $gender;
-
 
 
     $file = "./result.txt";
@@ -86,19 +79,21 @@ foreach ($array as $person) {
     $num4 += 1;
   }
 }
-echo "Male :".$num."\t Female : ".($counter-$num)."\n";
-echo "Yes :".$num2;
-echo "No :".($counter-$num3);
-echo "Yes :".$num4;
+// echo "Male :".$num."\t Female : ".($counter-$num)."\n";
+// echo "Yes :".$num2;
+// echo "No :".($counter-$num3);
+// echo "Yes :".$num4;
 // $t1 = ($array[0]+$happy);
 // $t2 = ($array[1] + $hunger);
 // $t3 = ($array[2] + $car);
 // $t4 = ($array[3] +$student);
 
-echo "<center>Result</center>";
-echo "<br/><center>People who are happy: "."</center>";
-// echo "<br/><center>People who are happy: ". $t2 ."</center>";
-// echo "<br/><center>People who are happy: ". $t3 ."</center>";
+echo "<center>Result</center>\n";
+echo "<center>People who did a survey :".$counter."</center>\n";
+echo "<center>Male :".$num."\t Female : ".($counter-$num)."</center>\n";
+echo "<center>People who are over 18 : Yes ". $num2 ."\t No : ".($counter-$num2)."</center>";
+echo "<center>People who are college student : Yes ". $num3 ."\t No : ".($counter-$num3)."</center>";
+echo "<center>People who like programming : Yes ". $num4 ."\t No : ".($counter-$num4)."</center>";
 // echo "<br/><center>People who are happy: ". $t4 ."</center>";
 // $file = fopen("result.txt","w") or die("can't open fine");
 // $final = ($t1 . "\n" . $t2 . "\n" . $t3 . "\n" . $t4 . "\n");
