d216f07a4b838786d8ef3b3cb7b95f5105478a95
winsonso <winsonsosingwai319@gmail.com>
2017-02-04 13:19:50 -0700

    week05

--- web/week05.php
+++ web/week05.php
@@ -29,19 +29,19 @@
 </form>
 <?php
 
-  $dbUser = 'tvykcavenuypkg';
-  $dbPassword = 'e4cd5d6eca8fa1f7d9ead148580cc0c1b30cde2f37f4276da626ce47275eba0c';
-  $dbName = 'd38uii2m3augn0';
-  $dbHost = 'ec2-54-225-122-119.compute-1.amazonaws.com';
-  $dbPort = '5432';
+  // $dbUser = 'tvykcavenuypkg';
+  // $dbPassword = 'e4cd5d6eca8fa1f7d9ead148580cc0c1b30cde2f37f4276da626ce47275eba0c';
+  // $dbName = 'd38uii2m3augn0';
+  // $dbHost = 'ec2-54-225-122-119.compute-1.amazonaws.com';
+  // $dbPort = '5432';
 
-  try {
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    echo "Opened database successfully\n";
-  } catch (PDOException $ex) {
-    echo "Error connecting to DB. Details: $ex";
-    die();
-  }
+  // try {
+  //   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  //   echo "Opened database successfully\n";
+  // } catch (PDOException $ex) {
+  //   echo "Error connecting to DB. Details: $ex";
+  //   die();
+  // }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // include 'dbstuff.inc';
