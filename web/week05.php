aff6e238a6e7189a09ec8f5334a6072ec948f92d
winsonso <winsonsosingwai319@gmail.com>
2017-02-04 12:03:13 -0700

    week05

--- web/week05.php
+++ web/week05.php
@@ -26,37 +26,12 @@
             <input type="text" name="searchval" id="searchval">
             <button name="submit" type="submit">Go!</button>
         </form> -->
-        <form action="search-database.php" method="post">
+        <form action="week05_results.php" method="post">
             Search: <input type="text" name="search" placeholder=" Search here ... "/>
             <input type="submit" value="Submit" />
         </form>
 
 
-    <?php 
-    // Search from MySQL database table
-    $search=$_POST['search'];
-    $query = $pdo->prepare("select * from scripture where book LIKE '%$search%' OR content LIKE '%$search%'  LIMIT 0 , 10");
-    $query->bindValue(1, "%$search%", PDO::PARAM_STR);
-    $query->execute();
-    // Display search result
-             if (!$query->rowCount() == 0) {
-            echo "Search found :<br/>";
-            echo "<table style=\"font-family:arial;color:#333333;\">";  
-                    echo "<tr><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">Book</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">where</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">Content</td></tr>";       
-                while ($results = $query->fetch()) {
-            echo "<tr><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;\">";      
-                    echo $results['book'];
-            echo "</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;\">";
-                    echo $results['chapter']. ':'. $results['verse'];
-            echo "</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;\">";
-                    echo "$".$results['content'];
-            echo "</td></tr>";        
-                }
-            echo "</table>";    
-            } else {
-                echo 'Nothing found';
-            }
-?>
 
 
 
--- web/week05_results.php
+++ web/week05_results.php
@@ -28,6 +28,32 @@ foreach ($db->query('SELECT * from scripture where id='.$_GET["id"]) as $row)
     print "<p><span id='scriptref'>$row[1] $row[2]:$row[3]</span> - \"$row[4]\"</p>\n\n";
 }
 ?>
+
+    <?php 
+    // Search from MySQL database table
+    $search=$_POST['search'];
+    $query = $pdo->prepare("select * from scripture where book LIKE '%$search%' OR content LIKE '%$search%'  LIMIT 0 , 10");
+    $query->bindValue(1, "%$search%", PDO::PARAM_STR);
+    $query->execute();
+    // Display search result
+             if (!$query->rowCount() == 0) {
+            echo "Search found :<br/>";
+            echo "<table style=\"font-family:arial;color:#333333;\">";  
+                    echo "<tr><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">Book</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">where</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">Content</td></tr>";       
+                while ($results = $query->fetch()) {
+            echo "<tr><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;\">";      
+                    echo $results['book'];
+            echo "</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;\">";
+                    echo $results['chapter']. ':'. $results['verse'];
+            echo "</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;\">";
+                    echo "$".$results['content'];
+            echo "</td></tr>";        
+                }
+            echo "</table>";    
+            } else {
+                echo 'Nothing found';
+            }
+?>
 <br/>
 <a href="week05.php">Back to Scripture Resources - Search Page</a>
 </body>
