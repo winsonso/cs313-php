1793a0434a53a34ab531596934078585f751d7d9
winsonso <winsonsosingwai319@gmail.com>
2017-02-04 11:54:32 -0700

    week05

--- web/week05.php
+++ web/week05.php
@@ -21,6 +21,46 @@
   </head>
   <body>
      <h1>Scripture Resources</h1>
+<!--         <form method="post" action="">
+            <label for="searchval">Enter search term:</label>
+            <input type="text" name="searchval" id="searchval">
+            <button name="submit" type="submit">Go!</button>
+        </form> -->
+        <form action="search-database.php" method="post">
+            Search: <input type="text" name="search" placeholder=" Search here ... "/>
+            <input type="submit" value="Submit" />
+        </form>
+
+
+    <?php 
+    // Search from MySQL database table
+    $search=$_POST['search'];
+    $query = $pdo->prepare("select * from scripture where book LIKE '%$search%' OR content LIKE '%$search%'  LIMIT 0 , 10");
+    $query->bindValue(1, "%$search%", PDO::PARAM_STR);
+    $query->execute();
+    // Display search result
+             if (!$query->rowCount() == 0) {
+            echo "Search found :<br/>";
+            echo "<table style=\"font-family:arial;color:#333333;\">";  
+                    echo "<tr><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">Book</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">where</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;background:#98bf21;\">Content</td></tr>";       
+                while ($results = $query->fetch()) {
+            echo "<tr><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;\">";      
+                    echo $results['book'];
+            echo "</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;\">";
+                    echo $results['chapter']. ':'. $results['verse'];
+            echo "</td><td style=\"border-style:solid;border-width:1px;border-color:#98bf21;\">";
+                    echo "$".$results['content'];
+            echo "</td></tr>";        
+                }
+            echo "</table>";    
+            } else {
+                echo 'Nothing found';
+            }
+?>
+
+
+
+
      <?php
       $statement = $db->prepare("SELECT book, chapter, verse, content FROM scripture");
       $statement->execute();
@@ -34,10 +74,6 @@
       }
       echo '</ul>';
 
-// foreach ($db->query('SELECT * from scripture where id= 1') as $row)
-// {
-//     echo "<p><span id='scriptref'>$row[1] $row[2]:$row[3]</span> - \"$row[4]\"</p>\n\n";
-// }
 
       ?>
   </body>
