c8fbaccee6f4c7b5fe141caddcc6a73c4d61dbde
winsonso <winsonsosingwai319@gmail.com>
2017-02-11 16:48:23 -0700

    week06

--- web/insertToDb.php
+++ web/insertToDb.php
@@ -1,10 +1,10 @@
-<?php
+
 
 // get the data from the POST
 $book = $_POST['txtBook'];
 $chapter = $_POST['txtChapter'];
 $verse = $_POST['txtVerse'];
-//$content = $_POST['txtContent'];
+$content = $_POST['txtContent'];
 // For debugging purposes, you might include some echo statements like this
 // and then not automatically redirect until you have everything working.
 // echo "book=$book\n";
@@ -12,46 +12,33 @@ $verse = $_POST['txtVerse'];
 // echo "verse=$verse\n";
 // echo "content=$content\n";
 // we could (and should!) put additional checks here to verify that all this data is actually provided
+
+  <!doctype html>
+  <html>
+  <head>
+  <meta charset="UTF-8">
+  <title>Insert</title>
+  </head>
+  
+  <body>
+<?php
 require("dbConnect.php");
-$db = get_db();
-try
-{
-	// Add the Scripture
-	// We do this by preparing the query with placeholder values
-	$query = 'INSERT INTO scripture(book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)';
-	$statement = $db->prepare($query);
-	// Now we bind the values to the placeholders. This does some nice things
-	// including sanitizing the input with regard to sql commands.
-	$statement->bindValue(':book', $book);
-	$statement->bindValue(':chapter', $chapter);
-	$statement->bindValue(':verse', $verse);
-	$statement->bindValue(':content', $content);
-	$statement->execute();
-	// get the new id
-	$scriptureId = $db->lastInsertId("scripture_id_seq");
-	// Now go through each topic id in the list from the user's checkboxes
-	// foreach ($topicIds as $topicId)
-	// {
-	// 	echo "ScriptureId: $scriptureId, topicId: $topicId";
-	// 	// Again, first prepare the statement
-	// 	$statement = $db->prepare('INSERT INTO scripture_topic(scriptureId, topicId) VALUES(:scriptureId, :topicId)');
-	// 	// Then, bind the values
-	// 	$statement->bindValue(':scriptureId', $scriptureId);
-	// 	$statement->bindValue(':topicId', $topicId);
-	// 	$statement->execute();
-	// }
-}
-catch (Exception $ex)
-{
-	// Please be aware that you don't want to output the Exception message in
-	// a production environment
-	echo "Error with DB. Details: $ex";
-	die();
-}
-// finally, redirect them to a new page to actually show the topics
-//header("Location: showTopics.php");
-//die(); // we always include a die after redirects. In this case, there would be no
-       // harm if the user got the rest of the page, because there is nothing else
-       // but in general, there could be things after here that we don't want them
-       // to see.
+  $con= get_db();
+      
+$book = $_POST['txtBook'];
+$chapter = $_POST['txtChapter'];
+$verse = $_POST['txtVerse'];
+$content = $_POST['txtContent'];
+
+      $sql = "INSERT INTO scripture (book, chapter, verse,content)
+  VALUES ('".$book."','".$chapter."','".$verse."','".$content."')";
+  if ($con->query($sql) === TRUE) {
+      echo "New record created successfully";
+  } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
+  $conn->close();
 ?>
+  </body>
+  </html>
