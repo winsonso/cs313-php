0deb921c084713ecc9aaba31cd2e1f33e9c02f13
winsonso <winsonsosingwai319@gmail.com>
2017-02-14 20:44:04 -0700

    initial project

---
+++ web/project/showCxt.php
@@ -0,0 +1,28 @@
+<?php
+require("dbConnect.php");
+$db = get_db();
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Content</title>
+    <link rel="stylesheet" href="">
+  </head>
+  <body>
+     <h1>Expense Tracker</h1>
+<?php
+     $sqlstring = 'SELECT * FROM record INNER JOIN account ON record.ac_id = account.id WHERE account.id = 1 ORDER BY id';
+
+    foreach ($db->query($sqlstring) as $row)
+    {
+    	for ($i = 0; $i< 11 ; $i ++) { 
+    		echo $row[i].' ';
+    	}
+    	
+    }
+
+?>
+
+
+  </body>
+ </html>
