526c43215eb0a8f5f6574ab939aa8f0cd59d5e04
winsonso <winsonsosingwai319@gmail.com>
2017-02-14 21:07:33 -0700

    initial project

--- web/project/showCxt.php
+++ web/project/showCxt.php
@@ -11,14 +11,16 @@ $db = get_db();
   <body>
      <h1>Expense Tracker</h1>
 <?php
-     $sqlstring = 'SELECT * FROM record INNER JOIN account ON record.ac_id = account.id WHERE account.id = 1 ORDER BY id';
+    $sqlstring = 'SELECT * FROM record INNER JOIN account ON record.ac_id = account.id WHERE account.id = 1 ORDER BY id';
 
+    echo "<table><tr><th>User</th><th>Date</th><th>Living Expense</th><th>Food Expense</th><th>Tithing</th><th>Others</th><th>Saving</th></tr>";
     foreach ($db->query($sqlstring) as $row)
     {
-    	echo "<table><tr><th>User</th><th>Date</th><th>Living Expense</th><th>Food Expense</th><th>Tithing</th><th>Others</th><th>Saving</th></tr></table>";
-
-    	echo $row['living_exp'] . ' ' . $row['tithing'] . ' '. $row['food_exp']. ' ' . $row['others'] . ' '. $row['saving'];
+    	echo "<tr><th>".$row['username']."</th><th>".$row['date']."</th><th>".$row['living_exp']."</th><th>".$row['food_exp']."</th><th>".."</th><th>".$row['tithing']."</th><th>".$row['others']."</th><th>".$row['saving']."</th></tr>";
+    	//echo $row['living_exp'] . ' ' . $row['tithing'] . ' '. $row['food_exp']. ' ' . $row['others'] . ' '. $row['saving'];
     }
+
+    echo "</table>";
 
 ?>
 
