9fa79d0a7bf9a5dabc99a9fb763e9d1920ae1ce3
winsonso <winsonsosingwai319@gmail.com>
2017-02-18 13:21:29 -0700

    project updated

--- web/project/insertToDb.php
+++ web/project/insertToDb.php
@@ -42,7 +42,8 @@ try
 {
 	// Add the Scripture
 	// We do this by preparing the query with placeholder values
-	$query = "INSERT INTO record (living_exp, tithing, food_exp, others, saving, ac_id, date)VALUES ('".$living_exp."','".$tithing."','".$food_exp."','".$others."','".$saving."','".$login_id."','".$date."')";
+
+	$query = "INSERT INTO record (living_exp, tithing, food_exp, others, saving, ac_id, date)VALUES (".$living_exp.",".$tithing.",".$food_exp.",".$others.",".$saving.",".$login_id.",'".$date."')";
 	$statement = $db->prepare($query);
 
 	$statement->execute();
@@ -56,7 +57,7 @@ catch (Exception $ex)
 	echo "Error with DB. Details: $ex";
 	die();
 }
-header("Location: showCxt.php");
-die();
+//header("Location: showCxt.php");
+//die();
 
 ?>
