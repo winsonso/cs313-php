86e057836fa2256646b062355fccb6bc904b1082
winsonso <winsonsosingwai319@gmail.com>
2017-02-18 10:44:26 -0700

    changed

--- web/team_week07/createAccount.php
+++ web/team_week07/createAccount.php
@@ -43,8 +43,8 @@ $statement = $db->prepare($query);
 // $statement->bindValue(':password', $hashedPassword);
 $statement->execute();
 // finally, redirect them to the sign in page
-header("Location: signIn.php");
-die(); // we always include a die after redirects. In this case, there would be no
+//header("Location: signIn.php");
+//die(); // we always include a die after redirects. In this case, there would be no
        // harm if the user got the rest of the page, because there is nothing else
        // but in general, there could be things after here that we don't want them
        // to see.
