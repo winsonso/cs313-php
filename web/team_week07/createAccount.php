d7232c612d4782cbfa57925b61b31777c9a2d7b4
winsonso <winsonsosingwai319@gmail.com>
2017-02-18 10:47:13 -0700

    changed

--- web/team_week07/createAccount.php
+++ web/team_week07/createAccount.php
@@ -34,14 +34,22 @@ echo "pw2=".$hashedPassword;
 
 require("dbConnect.php");
 $db = get_db();
-$query = "INSERT INTO account(username, password) VALUES('".$username."','".$hashedPassword."')";
-$statement = $db->prepare($query);
-// $statement->bindValue(':username', $username);
-// // **********************************************
-// // NOTICE: We are submitting the hashed password!
-// // **********************************************
-// $statement->bindValue(':password', $hashedPassword);
-$statement->execute();
+try
+{
+	$query = "INSERT INTO account(username, password) VALUES('".$username."','".$hashedPassword."')";
+	$statement = $db->prepare($query);
+	// $statement->bindValue(':username', $username);
+	// // **********************************************
+	// // NOTICE: We are submitting the hashed password!
+	// // **********************************************
+	// $statement->bindValue(':password', $hashedPassword);
+	$statement->execute();
+}
+catch (Exception $ex)
+{
+	echo "Error with DB. Details: $ex";
+	die();
+}
 // finally, redirect them to the sign in page
 //header("Location: signIn.php");
 //die(); // we always include a die after redirects. In this case, there would be no
