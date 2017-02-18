2b1b44e2194289ad881f71126af7fe681bba6220
winsonso <winsonsosingwai319@gmail.com>
2017-02-18 12:05:05 -0700

    changed

--- web/project/signOut.php
+++ web/project/signOut.php
@@ -6,7 +6,7 @@
 * Description: Clears the username from the session if there.
 *
 ***********************************************************/
-require("password.php"); // used for password hashing.
+//require("password.php"); // used for password hashing.
 session_start();
 unset($_SESSION['username']);
 header("Location: signIn.php");
