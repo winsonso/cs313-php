fe97bdeee86137d5f8d80b40d3740ef29d44d6c0
winsonso <winsonsosingwai319@gmail.com>
2017-02-18 11:59:08 -0700

    changed

--- web/project/signIn.php
+++ web/project/signIn.php
@@ -39,7 +39,7 @@ if (isset($_POST['txtUser']) && isset($_POST['txtPassword']))
 		{
 			// password was correct, put the user on the session, and redirect to home
 			$_SESSION['username'] = $username;
-			header("Location: home.php");
+			header("Location: showCxt.php");
 			die(); // we always include a die after redirects.
 		}
 		else
