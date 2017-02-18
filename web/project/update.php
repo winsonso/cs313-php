b9800e1b351446ec86c1ded2296ac9c5687d370c
winsonso <winsonsosingwai319@gmail.com>
2017-02-18 14:35:29 -0700

    project updated

--- web/project/update.php
+++ web/project/update.php
@@ -30,25 +30,25 @@ echo "record_id " . $record_id;
 require("dbConnect.php");
   $db= get_db();
 
-// try
-// {
-// 	// Add the Scripture
-// 	// We do this by preparing the query with placeholder values
-// 	$query = "UPDATE record SET living_exp='$living_exp', tithing='$tithing', food_exp='$food_exp', others='$others', saving='$saving', month='$month', year='$year' WHERE id='$record_id'";
-// 	$statement = $db->prepare($query);
+try
+{
+	// Add the Scripture
+	// We do this by preparing the query with placeholder values
+	$query = "UPDATE record SET living_exp='$living_exp', tithing='$tithing', food_exp='$food_exp', others='$others', saving='$saving', month='$month', year='$year' WHERE id='$record_id'";
+	$statement = $db->prepare($query);
 
-// 	$statement->execute();
-// 	echo "<strong><center>Updated record successfully!</center></strong>";
-// 	echo "<a href=\"showCxt.php\">Back to Record Page</a>";
+	$statement->execute();
+	echo "<strong><center>Updated record successfully!</center></strong>";
+	echo "<a href=\"showCxt.php\">Back to Record Page</a>";
 
-// }
-// catch (Exception $ex)
-// {
-// 	// Please be aware that you don't want to output the Exception message in
-// 	// a production environment
-// 	echo "Error with DB. Details: $ex";
-// 	die();
-// }
+}
+catch (Exception $ex)
+{
+	// Please be aware that you don't want to output the Exception message in
+	// a production environment
+	echo "Error with DB. Details: $ex";
+	die();
+}
 ?>
 </body>
 </html>
