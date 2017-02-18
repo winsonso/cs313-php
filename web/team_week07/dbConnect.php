1ab3060bf753fc0cfa6e2f511a64e4bb02db6593
winsonso <winsonsosingwai319@gmail.com>
2017-02-18 10:27:22 -0700

    team07

---
+++ web/team_week07/dbConnect.php
@@ -0,0 +1,19 @@
+<?php
+function get_db() {
+	$db = NULL;
+	$dbUser = 'tvykcavenuypkg';
+  	$dbPassword = 'e4cd5d6eca8fa1f7d9ead148580cc0c1b30cde2f37f4276da626ce47275eba0c';
+  	$dbName = 'd38uii2m3augn0';
+  	$dbHost = 'ec2-54-225-122-119.compute-1.amazonaws.com';
+  	$dbPort = '5432';
+
+	  try {
+	    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+	    //echo "Opened database successfully\n";
+	  } catch (PDOException $ex) {
+	    echo "Error connecting to DB. Details: $ex";
+	    die();
+	  }
+	  	return $db;
+}
+ ?>
